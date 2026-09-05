1e8e789cdf1971d41b0489a8ffe2bdf476b5a30c
Tuna Odabas <odabasgast@gmail.com>
2026-09-05 21:37:40 +0200

    Add firebase-config.example.js with instructions

---
+++ firebase-config.example.js
@@ -0,0 +1,11 @@
+// Copy this file to firebase-config.js and replace the placeholders with your Firebase Web app config
+// You can find these values in your Firebase console: Project settings -> General -> Your apps -> Firebase SDK snippet (Config)
+
+window.firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
